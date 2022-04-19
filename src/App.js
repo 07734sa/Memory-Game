26e3976bd9cc1c06f3320f7a25111b02dd7fa607
50773sa <93046590+50773sa@users.noreply.github.com>
2022-04-19 17:30:41 +0200

    Function for shuffle cards and counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
 
+const cardImg = [
+  {"src": "/img/helmet-1.png"},
+  {"src": "/img/potion-1.png"},
+  {"src": "/img/ring-1.png"},
+  {"src": "/img/scroll-1.png"},
+  {"src": "/img/shield-1.png"},
+  {"src": "/img/sword-1.png"},
+]
+
 function App() {
+  const [cards, setCards] = useState([])
+
+  // how many turns a user is taking to complete the game
+  const [turns, setTurns] = useState(0) // counter beginns at 0
+
+  // shuffle cards
+  const shuffle = () => {
+    const shuffledCards = [...cardImg, ...cardImg]    // duplicate cards
+      .sort(() => Math.random() - 0.5) // negative number = order remains, else switch them around
+      .map((card) => ({ ...card, id: Math.random()})) // add id for each card-object
+
+    setCards(shuffledCards) //update state
+    setTurns(0) // counter
+  }
+
+  console.log('Show cards: ', cards, 'show turns:', turns)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Memory</h1>
+      <button onClick={shuffle}>New Game</button>
+
+   
     </div>
   );
+  
 }
-
 export default App;
