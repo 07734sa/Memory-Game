bcbee013ad96e529cefb73932c1780c582e93e23
50773sa <93046590+50773sa@users.noreply.github.com>
2022-04-20 09:38:09 +0200

    New state for choice + function added. useEffect for comparing cards

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css';
+import SingleCard from './components/SingleCard';
 
 const cardImg = [
   {"src": "/img/helmet-1.png"},
@@ -12,40 +13,76 @@ const cardImg = [
 
 function App() {
   const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)  // how many turns a user is taking to complete the game // counter beginns at 0
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
 
-  // how many turns a user is taking to complete the game
-  const [turns, setTurns] = useState(0) // counter beginns at 0
+//------------------------------------------------------------
 
   // shuffle cards
   const shuffle = () => {
-    const shuffledCards = [...cardImg, ...cardImg]    // duplicate cards
-      .sort(() => Math.random() - 0.5) // negative number = order remains, else switch them around
-      .map((card) => ({ ...card, id: Math.random()})) // add id for each card-object
+		const shuffledCards = [...cardImg, ...cardImg]    // duplicate cards
+			.sort(() => Math.random() - 0.5) // negative number = order remains, else switch them around
+			.map((card) => ({ ...card, id: Math.random()})) // add id for each card-object
 
-    setCards(shuffledCards) //update state
-    setTurns(0) // counter
+		setCards(shuffledCards) //update state
+		setTurns(0) // counter
+  }
+  //console.log('Show cards: ', cards, 'show turns:', turns)
+
+  //-----------------------------------------------------------
+  
+  // handle choice
+  const handleChoice = (card)  => {
+		// om null = false så finns ett värde, annars true
+		choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
   }
 
-  console.log('Show cards: ', cards, 'show turns:', turns)
+	// ------------------------------------------------------------------------
+
+  //compare two cards. avfyras inte förrän något har ändras 
+	useEffect(() => {
+		if (choiceOne && choiceTwo) {
+
+			if (choiceOne.src === choiceTwo.src) {
+				console.log('Yey, they match!')
+				resetTurn()
+			} else {
+				console.log('No match :(')
+				setChoiceOne(null)
+				setChoiceTwo(null)
+				setTurns(prevTurns => prevTurns + 1)
+			}
+		}
+	}, [choiceOne, choiceTwo])
+
+	//---------------------------------------------------------------------------
+
+	// reset choices and increase turn with +1
+	const resetTurn = () => {
+		setChoiceOne(null)
+		setChoiceTwo(null)
+		setTurns(prevTurns => prevTurns + 1)
+	}
+	//---------------------------------------------------------------------------
+
 
   return (
-    <div className="App">
-      <h1>My Memory</h1>
-      <button onClick={shuffle}>New Game</button>
+		<div className="App">
+			<h1>My Memory</h1>
+			<button onClick={shuffle}>New Game</button>
 
-      <div className="card-grid">
-        {cards.map(card => (
-          <div className="card" key={card.id}> 
-            <div>
-              <img className="front" src={card.src} alt="card front"/> {/** front side */}
-              <img className="back" src="/img/cover.png" alt="card back" />
-            </div>
+			<div className="card-grid">
+				{cards.map(card => (
 
-          </div>
-
-        ))}
-      </div>
-    </div>
+					<SingleCard 
+						key={card.id} 
+						card={card} 
+						handleChoice={handleChoice}
+					/>
+				))}
+			</div>
+	</div>
   );
   
 }
